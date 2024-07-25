0d7b62d886983c241af9fa51e71840390c2cbbc0
Celina Chouali <celinachouali47@gmail.com>
2024-07-25 19:19:33 +0200

    BackLink

--- src/components/widgets/BackLink.jsx
+++ src/components/widgets/BackLink.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import * as all from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useNavigationHistory } from './Navigator';
+// import { useNavigationHistory } from './Navigator';
 
 const BackLink = ({ backlink, force = false }) => {
-    const { history, removeLastEntry } = useNavigationHistory();
-
-    const previousUrl = !force && (history.length > 1) && (history[history.length - 1] != history[history.length - 2])
-    ?  history[history.length - 2]
-    : backlink;
-
-    const handleClick = () => {
-        if (history.length > 1) {
-            removeLastEntry();
-            removeLastEntry();
+    const navigate = useNavigate();
+    const goBack = (e) => {
+        if(!force) {
+            e.preventDefault();
+            navigate(-1);
         }
     };
 
+    // const { history, removeLastEntry } = useNavigationHistory();
+
+    // const previousUrl = !force && (history.length > 1) && (history[history.length - 1] != history[history.length - 2])
+    // ?  history[history.length - 2]
+    // : backlink;
+
+    // const handleClick = () => {
+    //     if (history.length > 1) {
+    //         removeLastEntry();
+    //         removeLastEntry();
+    //     }
+    // };
+
     return (
-        <NavLink to={previousUrl} onClick={handleClick} style={{borderRadius: "50px", padding: "4px 10px", height: "100%"}}>
+        <NavLink to={backlink} onClick={goBack} style={{borderRadius: "50px", padding: "4px 10px", height: "100%"}}>
             <FontAwesomeIcon icon={all.faArrowLeft}/>
         </NavLink>
     );
